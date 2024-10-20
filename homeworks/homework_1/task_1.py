1d141d627f7b104c24a5b9510c2adf2ab299a21e
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2024-10-20 14:03:59 +0300

    restart lecture 1

---
+++ homeworks/homework_1/task_1.py
@@ -0,0 +1,28 @@
+width = int(input('Введите ширину рамки: '))
+heigth = int(input('Введите высоту рамки: '))
+
+for i in range(heigth):
+    if i == 0:
+        for k in range(width):
+            if k == 0:
+                print('|')
+            elif k > 0 and k < width - 1:
+                print('-')
+            else:
+                print('|')
+    elif i > 0 and i < heigth - 1:
+        for k in range(width):
+            if k == 0:
+                print('|')
+            elif k > 0 and k < width - 1:
+                print(' ')
+            else:
+                print('|')
+    else:
+        for k in range(width):
+            if k == 0:
+                print('|')
+            elif k > 0 and k < width - 1:
+                print('-')
+            else:
+                print('|')
